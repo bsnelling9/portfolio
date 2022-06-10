753e673a39b65cc2a069bdb9583fa06b00ca4a51
Brodie <brodiesnelling@gmail.com>
2022-06-10 12:18:16 -0400

    fixed typos

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Header } from "../../components/Header/Header";
-import { About } from "../../components/About/About";
 import { Hero } from "../../components/Hero/Hero";
 import { Projects } from "../../components/Projects/Projects";
 import './HomePage.scss';
