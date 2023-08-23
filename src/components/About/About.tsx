9dbe3a44725cfb5c118fdfe7d6defa340036e4bc
Brodie <brodiesnelling@gmail.com>
2023-08-23 15:01:04 -0400

    chore: updated about message

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import './About.scss';
+import React from "react";
+import "./About.scss";
 
 export const About: React.FC = () => {
-    return (
-        <div className="about">
-            <div className='about__wrapper'>
-                <h3 className="about__title">Hi, I'm Brodie Snelling</h3>
-                <div className='about__content'>
-                    <p className='about__text'>
-                    I have always enjoyed math and science because they allow me to use problem-solving and logic to answer questions. 
-                    This interest led me to pursue a degree in Mechanical Engineering. Although I enjoyed the program itself, the work 
-                    I did once employed did not exactly reflect what I was passionate about. Advanced software in the field has automated 
-                    most of the problem-solving aspects of the job, leaving 3D modeling to the engineers. I did enjoy the creativity of designing 
-                    products but was missing the critical thinking components. After 3 years of working as a mechanical engineer, 
-                    I decided that it was time for a change. 
-
-                    
-                    After work I spent time after work learning about data structures, algothiums 
-                    I attended BrainStations Web Devel
-                    </p>
-                </div>
-            </div>
+  return (
+    <div className="about">
+      <div className="about__wrapper">
+        <h3 className="about__title">Hi, I'm Brodie Snelling</h3>
+        <div className="about__content">
+          <p className="about__text">
+            I am a software developer with a background in mechanical
+            engineering, combining both my engineering experience with my
+            passion for problem-solving and innovation to craft efficient
+            solutions to software challenges. I have experience in both front-
+            and back-end development and thrive in applying analytical skills to
+            streamline complex processes and deliver optimal outcomes.
+          </p>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
